7daee103b2607e145cddf0eeaa362f5768c9beaa
Peter Rogers <peter.rogers@mtuity.com>
2014-02-22 00:58:51 -0600

    Displaying negative minutes properly.

--- js/filters.js
+++ js/filters.js
@@ -1,11 +1,13 @@
 angular.module('StopwatchFilters', []).filter('minutes', function() {
   return function(input) {
+  	var minusSign = (input < 0) ? "-" : "";
+  	input = Math.abs(input);
     var minutes = Math.floor(input/600);
     var seconds = Math.floor(input%600/10);
     var tenths = input%10;
     if (seconds < 10)
     	seconds = "0" + seconds;
-    return minutes + ":" + seconds + "." + tenths;
+    return minusSign + minutes + ":" + seconds + "." + tenths;
   };
 });
 
