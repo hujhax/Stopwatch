97dd804900c8708123ec37842fc90c199308bc61
Peter Rogers <peter.rogers@mtuity.com>
2014-02-21 21:47:20 -0600

    Shoehorning in a stopwatch that actually works.

--- js/angular-setup.js
+++ js/angular-setup.js
@@ -2,9 +2,51 @@
   'use strict';
 
   // create the angular app
-  angular.module('StopwatchApp', ['StopwatchControllers']);
+  angular.module('StopwatchApp', ['StopwatchControllers', 'stopwatch']);
 
   // set up dependency injection
   angular.module('StopwatchControllers', []);
+
+
+  angular
+  .module('stopwatch', [])
+  .constant('SW_DELAI', 100)
+  .factory('stopwatch', function (SW_DELAI,$timeout) {
+      var data = { 
+              value: 0,
+              laps: []
+          },
+          stopwatch = null;
+          
+      var start = function () {;
+          stopwatch = $timeout(function() {
+              data.value++;
+              start();
+          }, SW_DELAI);
+      };
+
+      var stop = function () {
+          $timeout.cancel(stopwatch);
+          stopwatch = null;
+      };
+
+      var reset = function () {
+          stop()
+          data.value = 0;
+          data.laps = [];
+      };
+
+      var lap = function () {
+          data.laps.push(data.value);
+      };
+
+      return {
+          data: data,
+          start: start,
+          stop: stop,
+          reset: reset,
+          lap: lap
+      };
+  });
 
 }());
--- js/stopwatch-controller.js
+++ js/stopwatch-controller.js
@@ -2,11 +2,13 @@
   'use strict';
 
   angular.module('StopwatchControllers')
- 	 .controller('StopwatchController', ['$scope', function($scope) {
+ 	 .controller('StopwatchController', ['$scope', 'stopwatch', function($scope, stopwatch) {
  	 	$scope.time = 10;
 
  	 	$scope.setTime = function(minutes) {
  	 		$scope.time = (minutes > 0) ? minutes : $scope.customTime;
  	 	};
+
+ 	 	$scope.myStopwatch = stopwatch;
 	}]);
 }());
