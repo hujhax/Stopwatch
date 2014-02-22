447a1ee22d94f44519d427ac28fd4ff372f9b37a
Peter Rogers <peter.rogers@mtuity.com>
2014-02-21 21:08:59 -0600

    Adding basic data binding.

---
+++ js/angular-setup.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+
+  // create the angular app
+  angular.module('StopwatchApp', ['StopwatchControllers']);
+
+  // set up dependency injection
+  angular.module('StopwatchControllers', []);
+
+}());
---
+++ js/stopwatch-controller.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  angular.module('StopwatchControllers')
+ 	 .controller('StopwatchController', ['$scope', function($scope) {
+ 	 	$scope.time = 10;
+	}]);
+}());
