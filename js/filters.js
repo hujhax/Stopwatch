03eb7ad668fda5bbf2a6c69858d87d9d5cf36f06
Peter Rogers <peter.rogers@mtuity.com>
2014-02-22 00:29:01 -0600

    Making the 'minutes' filter display tenths properly.

--- js/filters.js
+++ js/filters.js
@@ -1,9 +1,10 @@
 angular.module('StopwatchFilters', []).filter('minutes', function() {
   return function(input) {
     var minutes = Math.floor(input/600);
-    var seconds = Math.floor(input%600)/10;
+    var seconds = Math.floor(input%600/10);
+    var tenths = input%10;
     if (seconds < 10)
     	seconds = "0" + seconds;
-    return minutes + ":" + seconds;
+    return minutes + ":" + seconds + "." + tenths;
   };
 });
