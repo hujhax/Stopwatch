62a6a7d0040c7f2d7b2700ac017eca0a40400b4b
Peter Rogers <peter.rogers@mtuity.com>
2014-02-22 00:21:14 -0600

    Add a filter to display minutes properly.

--- js/angular-setup.js
+++ js/angular-setup.js
@@ -2,7 +2,7 @@
   'use strict';
 
   // create the angular app
-  angular.module('StopwatchApp', ['StopwatchControllers', 'stopwatch']);
+  angular.module('StopwatchApp', ['StopwatchControllers', 'stopwatch', 'StopwatchFilters']);
 
   // set up dependency injection
   angular.module('StopwatchControllers', []);
---
+++ js/filters.js
@@ -0,0 +1,9 @@
+angular.module('StopwatchFilters', []).filter('minutes', function() {
+  return function(input) {
+    var minutes = Math.floor(input/600);
+    var seconds = Math.floor(input%600)/10;
+    if (seconds < 10)
+    	seconds = "0" + seconds;
+    return minutes + ":" + seconds;
+  };
+});
