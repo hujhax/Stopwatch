5d1f82318faeb24a2b7660a5099d7c3e5de5f99a
Peter Rogers <peter.rogers@mtuity.com>
2014-02-26 03:02:59 -0600

    Adding simple key bindings (1 = 10 min, 5 = 5 min, 3 = 30 min, c/C = custom min, space/enter = toggle).

--- js/stopwatch-controller.js
+++ js/stopwatch-controller.js
@@ -2,8 +2,37 @@
   'use strict';
 
   angular.module('StopwatchControllers')
- 	 .controller('StopwatchController', ['$scope', 'stopwatch', function($scope, stopwatch) {
+ 	 .controller('StopwatchController', ['$scope', 'stopwatch', 'keyboardManager', function($scope, stopwatch, keyboardManager) {
  	 	$scope.myStopwatch = stopwatch;
  	 	$scope.customTime = '15';
+
+		keyboardManager.bind('1', function() {
+			$scope.myStopwatch.start(10);
+		});
+
+		keyboardManager.bind('3', function() {
+			$scope.myStopwatch.start(30);
+		});
+
+		keyboardManager.bind('5', function() {
+			$scope.myStopwatch.start(5);
+		});
+
+		keyboardManager.bind('c', function() {
+			$scope.myStopwatch.start($scope.customTime);
+		});
+
+		keyboardManager.bind('C', function() {
+			$scope.myStopwatch.start($scope.customTime);
+		});
+
+		keyboardManager.bind(' ', function() {
+			$scope.myStopwatch.toggle();
+		});
+
+		keyboardManager.bind('enter', function() {
+			$scope.myStopwatch.toggle();
+		});
+
 	}]);
 }());
