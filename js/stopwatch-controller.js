78104d642161b673d4c3454b39c07e8f7496428a
Peter Rogers <peter.rogers@mtuity.com>
2014-02-28 00:37:27 -0600

    Ignore key-bindings if we're typing in text.

--- js/stopwatch-controller.js
+++ js/stopwatch-controller.js
@@ -6,37 +6,39 @@
  	 	$scope.myStopwatch = stopwatch;
  	 	$scope.customTime = '15';
 
+ 	 	var inputDisabled = {'inputDisabled': true};
+
 		keyboardManager.bind('1', function() {
 			$scope.myStopwatch.start(10);
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind('3', function() {
 			$scope.myStopwatch.start(30);
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind('5', function() {
 			$scope.myStopwatch.start(5);
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind('c', function() {
 			$scope.myStopwatch.start($scope.customTime);
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind('C', function() {
 			$scope.myStopwatch.start($scope.customTime);
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind(' ', function() {
 			$scope.myStopwatch.toggle();
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind('enter', function() {
 			$scope.myStopwatch.toggle();
-		});
+		}, inputDisabled);
 
 		keyboardManager.bind('escape', function() {
 			$scope.myStopwatch.clear();
-		});
+		}, inputDisabled);
 
 	}]);
 }());
